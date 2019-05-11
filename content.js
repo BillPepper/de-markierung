4c960ac3183fd0a19863507270a6994d7f005427
Mattes Wenzel <mattes@artfacts.net>
2019-05-11 15:18:15 +0200

    strip useless tags and dynamically replace keywords

--- content.js
+++ content.js
@@ -1,24 +1,59 @@
-var arr = [['Wolf']]
-var body = $('#js-article-column')
-var innerHTML = body.html()
+var arrKeywords = [['Tempora', 'COOL'], ['Nulla', 'GREAT']]
+var arrBlacklistElements = ['head', 'meta', 'title', 'link', 'style', 'script']
 
-highlightWords()
+var article = $('#body')
+var innerHTML = article.html()
+
+let strippedElements = stripBlacklistedItems()
+refreshHighlightedKeywords()
+
+function refreshHighlightedKeywords() {
+  strippedElements.forEach(function(element) {
+    element.innerHTML = highlightKeywords(element.innerHTML, arrKeywords)
+  })
+}
+
+function stripBlacklistedItems() {
+  let arrRet = []
+  let allNodes = document.getElementsByTagName('*')
+  for (let i = 0; i < allNodes.length; i++) {
+    let cn = allNodes[i]
+    if (!elementIsBlacklisted(cn)) {
+      arrRet.push(allNodes[i])
+    }
+  }
+  return arrRet
+}
+
+function elementIsBlacklisted(element) {
+  ret = false
+  for (let i = 0; i < arrBlacklistElements.length; i++) {
+    if (element.tagName.toLowerCase() === arrBlacklistElements[i]) {
+      ret = true
+    }
+  }
+
+  return ret
+}
+
+function highlightKeywords(inText, keywords) {
+  for (let i = 0; i < keywords.length; i++) {
+    inText = inText.replace(
+      new RegExp(keywords[i][0], 'g'),
+      '<span style="background-color: red; color: #333">' +
+        keywords[i][1] +
+        '</span>'
+    )
+  }
+  return inText
+}
 
 chrome.runtime.onMessage.addListener(messageReceived)
 
 function messageReceived(message, sender, sendResponse) {
   console.log(message)
-  arr.push(message.txt)
-  highlightWords()
+  arrKeywords.push([message.txt, 'test'])
+  refreshHighlightedKeywords()
 }
 
-function highlightWords() {
-  arr.forEach(function(num) {
-    innerHTML = innerHTML.replace(
-      new RegExp(num, 'g'),
-      '<span style="background-color: red">' + num + '</span>'
-    )
-  })
-  body.html(innerHTML)
-  console.log('updated highlights')
 }

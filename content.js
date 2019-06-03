bf686b02b7cb13c3c2f432e52407e0d60dea31dc
Mattes Wenzel <mattes@artfacts.net>
2019-06-03 13:25:37 +0200

    rm overlay and use popup

--- content.js
+++ content.js
@@ -39,76 +39,28 @@ const highlightKeywords = (inText, keywords) => {
   return inText
 }
 
+const getKeywordsFromDB = () => {
+  // this should be returned from the backend later on
+  let tmpList = [['Lorem', 'test'], ['autem', 'foo']]
+  return tmpList
+}
+
 const messageReceived = (message, sender, sendResponse) => {
-  setMenuVisible(true)
-  currentFilter[0] = message.txt
-}
+  arrKeywords.push([
+    message.txt,
+    window.prompt('Womit soll das Wort ersetzt werden?', '')
+  ])
 
-const setMenuVisible = isVisable => {
-  let menugBackground = document.getElementById('de-markierung-menu')
-  menugBackground.style.display = isVisable ? 'block' : 'none'
-}
-
-const createMenu = () => {
-  let html =
-    '<h2 id="de-markierung-text"> \
-      Womit soll das Wort ersetzt werden? \
-    </h2> \
-    <div class="de-markierung-inputs"> \
-      <input class="input" id="wordInput" type="input"/> \
-      <input class="input" type="submit" value="Ersetzen"/> \
-    </div>'
-  let menu = document.createElement('div')
-  menu.id = 'de-markierung-menu'
-  menu.style =
-    'background-color: #000;background-color: rgb(0, 0, 0, 0.5); position: absolute; top: 0; left: 0;width: 100vw; height: 100vh; border-radius: 5px;'
-
-  let menuForm = document.createElement('form')
-  menuForm.id = 'menuForm'
-  menuForm.style =
-    'width: 400px; height: auto; ackground-color: #555;margin: auto;margin-top: 20%; padding: 20px;'
-
-  menuForm.addEventListener('submit', handleSubmit)
-
-  menuForm.innerHTML = html
-  menu.appendChild(menuForm)
-  document.body.appendChild(menu)
-}
-
-const handleSubmit = e => {
-  e.preventDefault()
-  setMenuVisible(false)
-  currentFilter[1] = e.target.wordInput.value
-  arrKeywords.push(currentFilter)
-  currentFilter = []
   refreshHighlightedKeywords()
-}
-
-debug = () => {
-  console.log('make post request')
-
-  const xhr = new XMLHttpRequest()
-  xhr.open('POST', 'http://localhost:3000/keywords', true)
-  xhr.setRequestHeader('Content-Type', 'application/json')
-  xhr.send(
-    JSON.stringify({
-      searchTerm: 'test',
-      replacement: 'test'
-    })
-  )
 }
 
 const init = () => {
   chrome.runtime.onMessage.addListener(messageReceived)
 
   refreshHighlightedKeywords()
-  createMenu()
-  setMenuVisible(false)
-
-  debug()
 }
 
-let arrKeywords = []
+let arrKeywords = getKeywordsFromDB()
 let currentFilter = []
 let arrBlacklistElements = ['head', 'meta', 'title', 'link', 'style', 'script']
 let strippedElements = stripBlacklistedItems()

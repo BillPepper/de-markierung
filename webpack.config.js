b1df2380b8360b64575f2ffe46dff39a97c28e9d
Mattes Wenzel <mattes@artfacts.net>
2019-06-05 15:59:36 +0200

    use webpack to bundle the plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,22 @@
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
 
 module.exports = {
   entry: {
-    contentBundle: './src/content.js',
-    backgroundBundle: './src/background.js'},
+    content: './src/content.js',
+    background: './src/background.js'},
   output: {
-    filename: '[name].bundle.js',
-    chunkFilename: '[name].bundle.js',
+    filename: '[name].js',
+    chunkFilename: '[name].js',
     path: path.resolve(__dirname, 'dist')
 
-  }
+  },
+    plugins: [
+      new CopyPlugin([
+        {from: './manifest.json', to: 'manifest.json'},
+        {from: './icon.png', to: 'icon.png'},
+        {from: './icon_128.png', to: 'icon_128.png'},
+      ])
+    ]
 };

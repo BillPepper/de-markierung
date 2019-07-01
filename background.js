90c29542788ebd6290a20a93e1a20f592eb1f3e5
Mattes Wenzel <mattes@artfacts.net>
2019-07-01 11:19:09 +0200

    add list as static data

--- background.js
+++ background.js
@@ -1,20 +1,20 @@
-const CORS_PROXY = 'https://cors-anywhere.herokuapp.com/' // FIXME: This is not meant for Productoin!
-const API_URL = 'http://de-markierung.herokuapp.com'
+// const CORS_PROXY = 'https://cors-anywhere.herokuapp.com/' // FIXME: This is not meant for Productoin!
+// const API_URL = 'http://de-markierung.herokuapp.com'
 
-const httpGet = theUrl => {
-  var xmlHttp = new XMLHttpRequest()
-  xmlHttp.open('GET', theUrl, false)
-  xmlHttp.send(null)
-  return xmlHttp.responseText
-}
+// const httpGet = theUrl => {
+//   var xmlHttp = new XMLHttpRequest()
+//   xmlHttp.open('GET', theUrl, false)
+//   xmlHttp.send(null)
+//   return xmlHttp.responseText
+// }
 
-const arrKeywords = JSON.parse(httpGet(CORS_PROXY + API_URL + '/maps'))
-const arrBlacklistWords = JSON.parse(
-  httpGet(CORS_PROXY + API_URL + '/blacklist')
-)
+// const arrKeywords = JSON.parse(httpGet(CORS_PROXY + API_URL + '/maps'))
+// const arrBlacklistWords = JSON.parse(
+//   httpGet(CORS_PROXY + API_URL + '/blacklist')
+// )
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request.message == 'get_keywords') sendResponse({ keywords: arrKeywords })
-  if (request.message == 'get_blacklist')
-    sendResponse({ blacklist: arrBlacklistWords })
-})
+// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+//   if (request.message == 'get_keywords') sendResponse({ keywords: arrKeywords })
+//   if (request.message == 'get_blacklist')
+//     sendResponse({ blacklist: arrBlacklistWords })
+// })
